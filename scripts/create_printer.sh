faaadaa54e7fe5c81ed0ce713d5958279ce97705
Murilolana <murilo@protomak.com.br>
2023-05-05 12:18:52 -0300

    added backup-variables.cfg with the updated script

--- scripts/create_printer.sh
+++ scripts/create_printer.sh
@@ -3,15 +3,19 @@
 echo "
 [Syncraft Printer CFG File]
 
-Do you want to copy the \"backup-printer.cfg\" file
-to the new location, with the new name \"printer.cfg\"?
+Do you want to copy both the \"backup-printer.cfg\" file
+and the \"backup-variables.cfg\" file to the new location,
+with the new respective names?
 
-The file will be copied from
-/home/pi/printer_data/config/backup-printer.cfg
+The files will be copied from
+> /home/pi/printer_data/config/backup-printer.cfg
+> /home/pi/printer_data/config/backup-variables.cfg
 to
-/home/pi/printer_data/config/printer.cfg
+> /home/pi/printer_data/config/printer.cfg
+> /home/pi/printer_data/config/variables.cfg
 
 [!] IF A \"printer.cfg\" FILE ALREADY EXISTS, IT WILL BE OVERWRITTEN!
+[!] IF A \"variables.cfg\" FILE ALREADY EXISTS, IT WILL BE OVERWRITTEN!
 
 [*] Type \"GO\" to perform this action, and \"QUIT\" to exit the script.
 "
@@ -21,10 +25,13 @@ read input
 if [[ $input == "GO" || $input == "go" ]]; then
     echo -e "\n\n"
     cp /home/pi/printer_data/config/backup-printer.cfg /home/pi/printer_data/config/printer.cfg
+    cp /home/pi/printer_data/config/backup-variables.cfg /home/pi/printer_data/config/variables.cfg
     chown pi /home/pi/printer_data/config/printer.cfg
+    chown pi /home/pi/printer_data/config/variables.cfg
     echo "[*] The new printer.cfg file has been created."
-    echo "[*] File owner defined as \"pi\"."
-    echo "[*] FINISHED."
+    echo "[*] The new variables.cfg file has been created."
+    echo "[*] File owner defined as \"pi\" for both files."
+    echo "[*] DONE."
 elif [[ $input == "QUIT" || $input == "quit" ]]; then
     echo -e "\n\n"
     echo "[*] NO CHANGES HAVE BEEN MADE."
