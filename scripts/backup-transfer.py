1cf216e5f209a847ba2e5cdc7c0f3caa979853d7
rafaelSwi <rafanswi@protonmail.com>
2023-08-01 18:00:26 -0300

    added useful comments to the transfer.py script

--- scripts/backup-transfer.py
+++ scripts/backup-transfer.py
@@ -1,6 +1,12 @@
 import os
 import shutil
 
+################################################################
+###### REMEMBER TO TURN THE BACKUP FILE INTO A CLEAN FILE ######
+################################################################
+######   /home/pi/printerdataconfig/scripts/transfer.py   ######
+################################################################
+
 def transfer_files(source_dir, destination_dir, block_list):
     for root, _, files in os.walk(source_dir):
         for file in files:
@@ -13,8 +19,23 @@ def transfer_files(source_dir, destination_dir, block_list):
                 shutil.copy2(src_file, dst_file)
 
 if __name__ == "__main__":
-    source_directory = "/path/" 
-    destination_directory = "/path/" 
+
+    # The printerdataconfig path that will be updated over the Internet
+    # It will probably be "/home/pi/printerdataconfig"
+    source_directory = "/path/"
+
+    # The printerdataconfig path that will be used for Klipper
+    # It will probably be "/home/pi/printer_data/config"
+    destination_directory = "/path/"
+
+    ############################################################################
+    ###### INSERT IN THE ARRAY WHAT SHOULD BE BLOCKED DURING THE TRANSFER ######
+    ############################################################################
+    ###### * Files must have the extension                                ######
+    ###### * Folders can be referenced                                    ######
+    ###### * Subfolders cannot be referenced                              ######
+    ############################################################################
+
     block = [""]
 
     transfer_files(source_directory, destination_directory, block)
