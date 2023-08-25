445762459f0129518233ec68bc971865ade85f47
Murilolana <murilo@protomak.com.br>
2023-08-25 13:44:00 -0300

    fixed 777 perms. in helper

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -238,9 +238,4 @@ echo "false" > legacy.txt
 sudo chmod 777 ~/printerdataconfig/legacy.txt
 echo "[HELPER] DONE: $process."
 
-process='Add "777" permission on RPy Brightness File.'
-echo "[HELPER] START: $process."
-sudo chmod 777 /sys/class/backlight/*/brightness
-echo "[HELPER] DONE: $process."
-
 echo -e "\n\n[HELPER] DONE."
