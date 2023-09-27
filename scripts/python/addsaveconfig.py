88e821952ffc5b29c2c4d08bc48886f3a2492883
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-09-27 11:37:12 -0300

    'addsaveconfig' now use backup if error in extract

--- scripts/python/addsaveconfig.py
+++ scripts/python/addsaveconfig.py
@@ -1,54 +1,97 @@
 import os
 
-default_path: str = "/home/pi/printer_data/config"
+default_path = os.path.join('/home', 'pi', 'printer_data', 'config')
+new_path = os.path.join('/home', 'pi', 'printerdataconfig')
 
-source_file_path = f"{default_path}/printer.cfg"
-saveconfig_file_path = f"{default_path}/backups/backup-printercfgsaveconfig.txt"
-new_file_path = f"{default_path}/printer.cfg"
-legacy_file_path = "/home/pi/printerdataconfig/legacy.txt"
-extracted_saveconfig_path = "/home/pi/printerdataconfig/backups/extract-saveconfig.txt"
+class PATH:
+    SOURCE = os.path.join(default_path, 'printer.cfg')
+    SAVECONFIG = os.path.join(default_path, 'backups', 'backup-printercfgsaveconfig.txt')
+    LEGACY = os.path.join(new_path, 'legacy.txt')
+    EXTRACTED = os.path.join(new_path, 'backups', 'extract-saveconfig.txt')
+    MOONRAKER = os.path.join(default_path, 'moonraker.conf')
 
-if os.path.exists(extracted_saveconfig_path):
-    saveconfig_file_path = f"{extracted_saveconfig_path}"
+saveconfig_line: str = '#*# <---------------------- SAVE_CONFIG ---------------------->'
+saveconfig_backup: str = """
+#*# <---------------------- SAVE_CONFIG ---------------------->
+#*# DO NOT EDIT THIS BLOCK OR BELOW. The contents are auto-generated.
+#*#
+#*# [probe]
+#*# z_offset = -0.300
+#*#
+#*# [stepper_z]
+#*# position_endstop = 340.25
+"""
 
-script_name = "[ADD SAVE CONFIG]"
+if os.path.exists(PATH.EXTRACTED):
+    PATH.SAVECONFIG = f"{PATH.EXTRACTED}"
 
-with open(source_file_path, 'r') as source_file:
+name = "[ADD SAVE CONFIG]"
+
+with open(PATH.SOURCE, 'r') as source_file:
     source_content = source_file.readlines()
-    print (f"{script_name} source file path: {source_file_path}.")
+    print (f"{name} source file path: {PATH.SOURCE}.")
 
-with open(saveconfig_file_path, 'r') as saveconfig_file:
+with open(PATH.SAVECONFIG, 'r') as saveconfig_file:
     saveconfig_content = saveconfig_file.readlines()
-    print (f"{script_name} saveconfig file path: {saveconfig_file_path}.")
+    print (f"{name} saveconfig file path: {PATH.SAVECONFIG}.")
 
-combined_content = source_content + ["\n", "\n"] + saveconfig_content
-print (f"{script_name} the new file was built.")
+    duplicateSaveConfig: bool = False
+    hasSaveConfigLine: bool = False
 
-with open(new_file_path, 'w') as new_file:
-    new_file.writelines(combined_content)
-    print (f"{script_name} the new file replaced the source file.")
-    print (f"{script_name} OK.")
+    for line in source_content:
+        if saveconfig_line in line:
+            duplicateSaveConfig = True
+
+    for line in saveconfig_content:
+        if saveconfig_line in line:
+            print (f"{name} 'Save Config Line' found in extracted file.")
+            hasSaveConfigLine = True
+
+    if not hasSaveConfigLine:
+        print (f"{name} 'Save Config Line' not found in extracted file. Using backup...")
+        saveconfig_content = []
+        for i, line in enumerate(saveconfig_backup):
+            try:
+                saveconfig_content.append(saveconfig_backup[i])
+            except:
+                pass
+
+combined_content = source_content + ["\n"] + saveconfig_content
+
+if not duplicateSaveConfig:
+    with open(PATH.SOURCE, 'w') as new_file:
+        new_file.writelines(combined_content)
+        print (f"{name} the new file replaced the source file.")
+else:
+    print (f"{name} duplicate saveconfig line detected. skip merge.")
 
 def replace_and_overwrite(file_path):
     with open(file_path, "r") as file:
         content = file.read()
     
-    new_content = content.replace("~/printer_data/config", "~/printerdataconfig")
+    new_content = content.replace(default_path, new_path)
     
     with open(file_path, "w") as file:
         file.write(new_content)
 
-if os.path.exists(legacy_file_path):
-    with open(legacy_file_path, "r") as file:
+if os.path.exists(PATH.LEGACY):
+    with open(PATH.LEGACY, "r") as file:
         file_content = file.read().strip()
     
     if file_content == "false":
-        replace_and_overwrite("/home/pi/printer_data/config/moonraker.conf")
-        print(f"{script_name} replaced printerdataconfig moonraker path.")
+        replace_and_overwrite(PATH.MOONRAKER)
+        print(f"{name} replaced printerdataconfig moonraker path.")
     else:
-        print(f"{script_name} system detected as legacy.")
+        print(f"{name} system detected as legacy.")
 else:
-    replace_and_overwrite("/home/pi/printer_data/config/moonraker.conf")
-    print(f"{script_name} replace printerdataconfig moonraker path.")
-    print(f"{script_name} legacy file don't exist. acting as non-legacy system")
-    print(f"{script_name} replaced printerdataconfig moonraker path.")
+    replace_and_overwrite(PATH.MOONRAKER)
+    print(f"{name} replace printerdataconfig moonraker path.")
+    print(f"{name} legacy file don't exist. acting as non-legacy system")
+    print(f"{name} replaced printerdataconfig moonraker path.")
+
+try:
+    os.system('sudo service klipper restart')
+    print(f"{name} klipper service restarted.")
+except:
+    print(f"{name} can't restart klipper service.")
+    pass
